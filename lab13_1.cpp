1193ea9d8816fa8c4702808667367c4d55fe5d37
playLoL <playza.1987@gmail.com>
2026-01-09 13:47:13 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,27 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double A[],int N,double B[]){
+    double sum = 0;
+    double sumSquares = 0;
+    double product = 1;
+    double sumReciprocals = 0;
+    double maxVal = A[0];
+    double minVal = A[0];
+    
+    for(int i = 0; i < N; i++){
+        sum += A[i];
+        sumSquares += A[i] * A[i];
+        product *= A[i];
+        sumReciprocals += 1.0 / A[i];
+        if(A[i] > maxVal) maxVal = A[i];
+        if(A[i] < minVal) minVal = A[i];
+    }
+    
+    B[0] = sum / N;
+    B[1] = sqrt(sumSquares / N - (sum / N) * (sum / N));
+    B[2] = pow(product, 1.0/N);
+    B[3] = N / sumReciprocals;
+    B[4] = maxVal;
+    B[5] = minVal;
+}
